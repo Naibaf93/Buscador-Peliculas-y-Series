674cabbee44e2510403447f13805320b1ea4d7e0
Fabián Huertas <miloh9318@hotmail.com>
2025-06-06 08:55:44 -0500

    creando dos funciones cambiarArchivo y mensajeModo y dos escuchadores de eventos

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ let boton = document.getElementById('miBoton');
 let lista = document.getElementById('miListado');
 
 let archivo = 'peliculas.json';
+
+//Escuchadores de eventos
+selector.addEventListener('change', cambiarArchivo);
+selector.addEventListener('cambioModo', mensajeModo);
+
+
+// Funciones
+function cambiarArchivo(){
+    archivo = selector.value;
+    let evento = new CustomEvent('cambioModo');
+}
+
+function mensajeModo(){
+    alert("el archivo de busqueda es " + selector.value);
+}
