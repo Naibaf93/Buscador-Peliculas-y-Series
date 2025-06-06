6f96e4caed438586583ab3f6462e504c0f689af1
Fabián Huertas <miloh9318@hotmail.com>
2025-06-05 21:11:15 -0500

    puliendo estilos

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@
 let selector = document.getElementById('miSelector');
 let input = document.getElementById('miInput');
 let boton = document.getElementById('miBoton');
-let lista = document.getElementById('miListado');
+let lista = document.getElementById('miListado'); 
