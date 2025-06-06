0ca38fe698217a5abfe5ad0bb5345a34df41a194
Fabián Huertas <miloh9318@hotmail.com>
2025-06-06 10:20:10 -0500

    creando el ultimo evento que era para el boton y la funcion buscar

--- js/app.js
+++ js/app.js
@@ -10,11 +10,13 @@ let archivo = 'peliculas.json';
 selector.addEventListener('change', cambiarArchivo);
 selector.addEventListener('cambioModo', mensajeModo);
 input.addEventListener('keydown', verificarInput);
+boton.addEventListener('click', buscar);
 
 // Funciones
 function cambiarArchivo(){
     archivo = selector.value;
     let evento = new CustomEvent('cambioModo');
+    selector.dispatchEvent(evento);
 }
 
 function mensajeModo(){
@@ -25,4 +27,40 @@ function verificarInput(e){
     if((e.keyCode < 65 || e.keyCode > 90) && e.keyCode != 32 && e.keyCode != 8){
         e.preventDefault();
     }
+}
+
+function buscar(){
+    lista.innerHTML = "";
+    
+    fetch(archivo)
+    .then(respuesta => respuesta.json())
+    .then(function(salida) {
+        for(let item of salida.data){
+            if(item.nombre.startsWith(input.value.toUpperCase())){
+                let p = document.createElement('p');
+                p.id = item.nombre;
+                p.innerHTML = item.sinopsis;
+                p.style.display = "none";
+
+                let li = document.createElement('li');
+                li.innerHTML = item.nombre;
+                li.addEventListener('mouseover', function() {
+                    let p = document.getElementById(item.nombre);
+                    p.style.display = 'block';
+                });
+
+                li.addEventListener('mouseout', function(){
+                    let p = document.getElementById(item.nombre);
+                    p.style.display = 'none';
+                });
+
+                li.appendChild(p);
+                lista.appendChild(li);
+            }
+        }
+    })
+
+    .catch(function(error) {
+        console.log(error);
+    })
 }
