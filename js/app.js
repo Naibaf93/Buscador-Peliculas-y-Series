d7af9373d848ef6d87cc00a61cec70db26d1dc1c
Fabián Huertas <miloh9318@hotmail.com>
2025-06-06 08:34:24 -0500

    creando la variable archivo

--- js/app.js
+++ js/app.js
@@ -2,4 +2,6 @@
 let selector = document.getElementById('miSelector');
 let input = document.getElementById('miInput');
 let boton = document.getElementById('miBoton');
-let lista = document.getElementById('miListado'); 
+let lista = document.getElementById('miListado');
+
+let archivo = 'peliculas.json';
